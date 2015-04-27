8eebca6d4cbe06516a1a7e80a5b5c4f66ada73e9
David Schmidt <mail@davidschmidt.at>
2015-04-27 12:16:04 +0200

    first code + tests

---
+++ lib/Parqus.pm
@@ -0,0 +1,129 @@
+package Parqus;
+use Moo;
+use namespace::autoclean;
+use Regexp::Grammars;
+
+# ABSTRACT: parse a search query string
+
+=head1 SYNOPSIS
+
+    use Parqus;
+    my $parser = Parqus->new( keywords => [ qw/name title/ ] );
+    my $res    = $parser->process('title:"bar baz" foo title: meh');
+    # {
+    #   words    => ['foo'],
+    #   keywords => {
+    #                 title => [
+    #                            'bar baz',
+    #                            'meh',
+    #                          ]
+    #               },
+    # }
+
+=head1 DESCRIPTION
+
+Parqus (PArse QUery String) parses a search-engine like string into a perl structure
+
+=head1 SEE ALSO
+
+L<Regexp::Grammars>
+
+=cut
+
+has keywords => (
+    is  => 'lazy',
+    isa => sub {
+        "$_[0] is not a HashRef!"
+          unless ref $_[0] eq 'HASH';
+    },
+    coerce => sub {
+        if ( ref $_[0] eq 'ARRAY' ) {
+            my %hash = map { $_ => 1 } @{ $_[0] };
+            return \%hash;
+        }
+    },
+    default => sub { {} },
+);
+
+has parser => (
+    is       => 'lazy',
+    init_arg => undef,
+    isa      => sub {
+        "$_[0] is not a Regexp!"
+          unless ref $_[0] eq 'Regexp';
+    },
+);
+my %keywords = ( title => 1, name => 1 );
+
+sub _build_parser {
+    my ($self) = @_;
+
+    #my %keywords = %{ $self->keywords };
+    return qr/
+                    <timeout: 3>
+                    ^
+                    \s*
+                    <[query]>*
+                    \s*
+                    $
+                    <rule: query>
+                        <item>|<item><query>
+                    <rule: item>
+                        <keyvalue>|<word>|<word= quote>
+                    <rule: word>
+                        <MATCH= ([\w]+)>
+                    <rule: keyvalue>
+                        <key>:<value>?
+                    <rule: key>
+                        <%keywords>
+                    <rule: value>
+                        <str= quote>|<str= word>
+                    <token: delim>
+                        ['"]
+                    <token: quote>
+                        <ldelim=delim><MATCH= (.*?)><rdelim=\_ldelim>
+                 /xms;
+
+}
+
+sub process {
+    my ( $self, $query ) = @_;
+
+    my $parser = $self->parser;
+
+    my %keywords = ();
+    my @words    = ();
+    my @errors   = ();
+    if ( $query =~ $self->parser ) {
+        for my $item ( @{ $/{query} } ) {
+            if ( exists $item->{item}{keyvalue} ) {
+                my $key = $item->{item}{keyvalue}{key};
+                my $value =
+                  exists $item->{item}{keyvalue}{value}
+                  ? $item->{item}{keyvalue}{value}{str}
+                  : '';
+                push( @{ $keywords{$key} }, $value );
+            }
+            elsif ( exists $item->{item}{word} ) {
+                push( @words, $item->{item}{word} );
+            }
+            else {
+                push( @errors, "Parse Error: neither word nor keyvalue" );
+            }
+        }
+    }
+    else {
+        push( @errors, "Parse Error: Invalid search query." );
+    }
+
+    push( @errors, @! )
+      if @!;
+
+    return {
+        ( scalar @words ? (words => \@words) : () ),
+        ( scalar keys %keywords ? (keywords => \%keywords) : () ),
+        ( scalar @errors ? (errors => \@errors) : () ),
+    };
+}
+
+1;
---
+++ t/01_basic.t
@@ -0,0 +1,43 @@
+#!/usr/bin/env perl
+use FindBin qw/$Bin/;
+use lib "$Bin/lib";
+use Test::More;
+use Test::Warnings;
+
+BEGIN { use_ok('Parqus'); }
+
+{
+    my $parser = Parqus->new();
+    ok( $parser, 'got a Parqus instance without passing keywords.' );
+}
+
+my $parser = Parqus->new( keywords => [qw/ title name /] );
+ok( $parser, 'got a Parqus instance with passing keywords.' );
+
+{
+    my $res = $parser->process('');
+    is_deeply( $res, {}, 'parse empty string.' );
+}
+{
+    my $res = $parser->process('foo');
+    is_deeply( $res, { words => ['foo'] }, 'parse single word.' );
+}
+{
+    my $res = $parser->process('title: foo');
+    is_deeply( $res, { keywords => { title => ['foo'] } }, 'parse single keyword.' );
+}
+{
+    my $res = $parser->process('"foo bar"');
+    is_deeply( $res, { words => ['foo bar'] }, 'parse quoted words.' );
+}
+{
+    my $res = $parser->process('name:"foo bar"');
+    is_deeply( $res, { keywords => { name => ['foo bar'] } }, 'parse keyword with quoted value.' );
+}
+{
+    my $res = $parser->process('&&');
+    ok( exists $res->{errors}, 'unquoted special chars causes error.');
+    is( $res->{errors}[0], 'Parse Error: Invalid search query.', 'found parse error.' );
+}
+
+done_testing();
